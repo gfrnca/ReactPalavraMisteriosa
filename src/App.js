2aafb1dd90c63c9d89a4c575f9498d9e4136694f
gfrnca <gabrielbfranca27@gmail.com>
2022-12-06 20:51:46 -0300

    feat(project): created and improved components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
+// CSS
 import './App.css';
+
+// React 
+import { useCallback, useEffect, useState } from 'react';
+
+// Data
+import { wordsList } from './data/words';
+
+// Components
 import StartScreen from './components/StartScreen/StartScreen';
+import GameScreen from './components/GameScreen/GameScreen';
+
+const stages = [
+  { id: 1, name: "start" },
+  { id: 2, name: "game" },
+  { id: 3, name: "end" },
+]
 
 function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name);
+
   return (
     <div className="App">
-      <StartScreen />
+      { gameStage === "start" && <StartScreen />}
+      { gameStage === "game" && <GameScreen />}
+      { gameStage === "end" && <StartScreen />}
     </div>
   );
 }
--- src/components/StartScreen/StartScreen.js
+++ src/components/StartScreen/StartScreen.js
@@ -2,7 +2,7 @@ import './StartScreen.css';
 
 const StartScreen = () => {
   return (
-    <div>
+    <div className="start-screen">
       <h1>Palavra Misteriosa</h1>
 
       <p>Clique no botão para começar o jogo</p>
