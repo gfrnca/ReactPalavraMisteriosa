c54f53249ee4f16fb5d2a2f478212715ec02b0cf
gfrnca <gabrielbfranca27@gmail.com>
2022-12-06 21:54:01 -0300

    feat(GameScreen): finished GameScreen page strucutre

--- src/components/GameScreen/GameScreen.js
+++ src/components/GameScreen/GameScreen.js
@@ -4,10 +4,31 @@ import './GameScreen.css';
 const GameScreen = ({ verifyLetter }) => {
   return (
     <div className="screen">
-      <h1>Acerte a palavra!</h1>
-
-      <p>Clique no botão para começar o jogo</p>
-      <button onClick={verifyLetter}>Finalizar jogo</button>
+      <div className="game">
+        <p className="points">
+          <span>Pontuação: 000</span>
+        </p>
+        <h1>Adivinhe a palavra:</h1>
+        <h3 className="tip">
+          Dica sobre a resposta: <span>Dica...</span>
+        </h3>
+        <div className="word-container">
+          <span className="letter">A</span>
+          <span className="blank-square"></span>
+        </div>
+        <div className="letter-container">
+          <p>Tente adivinhar uma letra da palavra: </p>
+          <form>
+            <input type="text" name="letter" maxLength="1" required />
+            <button>Checar</button>
+          </form>
+        </div>
+        <div className="wrong-letters">
+          <p>Letras já utilizadas</p>
+          <span>a, </span>
+          <span>b, </span>
+        </div>
+      </div>
     </div>
   )
 }
