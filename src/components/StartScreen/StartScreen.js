9fa8fbbe93b71d47435ff71c4ec61d377a28078a
gfrnca <gabrielbfranca27@gmail.com>
2022-12-06 21:13:05 -0300

    feat(StartScreen): now clicking in button will change game stage

--- src/components/StartScreen/StartScreen.js
+++ src/components/StartScreen/StartScreen.js
@@ -1,12 +1,12 @@
 import './StartScreen.css';
 
-const StartScreen = () => {
+const StartScreen = ({ startGame }) => {
   return (
     <div className="start-screen">
       <h1>Palavra Misteriosa</h1>
 
       <p>Clique no botão para começar o jogo</p>
-      <button>Começar o jogo</button>
+      <button onClick={startGame}>Começar o jogo</button>
     </div>
   )
 }
